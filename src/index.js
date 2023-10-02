55cb38f5b952e507ba574b410f40be34f509ff90
WessCoby <cobygiven@gmail.com>
2023-10-02 12:25:10 +0000

    Changed page link

--- src/index.js
+++ src/index.js
@@ -2,24 +2,9 @@ const jquery = require('jquery');
 const axios = require('axios');
 const { JSDOM } = require('jsdom');
 
-/**
- * Scenario
- * You are tasked with scraping data from a single web page and
- * presenting it in a structured format.
- * The web page contains a list of products with their names,
- * prices, and ratings.
- *
- * Example product:
- * {
- * 	 name: 'Nike Air Max',
- * 	 price: '25.00',
- *   rating: '4.5'
- * }
- */
-
 const getProducts = async () => {
 	// JQuery Instance
-	const { data } = await axios.get('https://scrape.wdemo.xyz');
+	const { data } = await axios.get('https://products.wdemo.xyz');
 	const dom = new JSDOM(data);
 	const $ = jquery(dom.window.parent, true);
 
